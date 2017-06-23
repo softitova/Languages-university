6b1eb382e02a4373734deaadee29acf7158711af
Titova Sophia <sof_titova@rambler.ru>
2017-06-23 11:56:51 +0300

    Delete http_utils.h

--- proxy_server_s/proxy_server_s/http_utils.h
+++
@@ -1,3 +0,0 @@
-const std::string BAD_REQUEST = "HTTP/1.1 400 Bad Request\r\nServer: proxy\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: 164\r\nConnection: close\r\n\r\n<html>\r\n<head><title>400 Bad Request</title></head>\r\n<body bgcolor=\"white\">\r\n<center><h1>400 Bad Request</h1></center>\r\n<hr><center>proxy</center>\r\n</body>\r\n</html>";
-
-static const time_t TIMEOUT = 10000;
